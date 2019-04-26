5d2a419cb62963e52f4c732b08d91f7dc2021509
CAWilson94 <charlotteawilson.94@gmail.com>
2019-04-26 11:17:35 +0100

    git globals for config ignored

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import tweepy 
+from gotListener import GoTListener
 
-consumer_key = 'key here'
-consumer_secret = 'key here'
-access_token = 'key here'
-access_token_secret = 'key here'
+consumer_key = 'uKvHiugPJ7zYeDgWRddLDCgVf'
+consumer_secret = 'Msx82y6h4MdAkoCXtSSdDUDWorLhebVf8cByA0ASDgN3AShzBX'
+access_token = '216437150-YKQh9phgT8L0ot2cm1WhQJJfthnMxhO3xYmlRQ8E'
+access_token_secret = 'rhJgB2rQCQ2BOpMJe9PuNhvMOJtD2ECeynzz7HMpIfNfV'
 
 # TODO: pop the keys into some other nice place instead of hard coding. Thanks. 
 
@@ -13,3 +14,6 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # TODO: shove a stream listener class in another file keep connections open for term frequencies. 
+
+twitter_stream = tweepy.Stream(auth, GoTListener())
+twitter_stream.filter(track=['#gameofthrones'])
