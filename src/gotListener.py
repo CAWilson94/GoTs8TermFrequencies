e0d3637208e4efadcf4f499d5f9c1c34c28880e3
CAWilson94 <charlotteawilson.94@gmail.com>
2019-04-26 11:18:06 +0100

    got listener

---
+++ src/gotListener.py
@@ -0,0 +1,18 @@
+from tweepy import Stream 
+from tweepy.streaming import StreamListener 
+
+
+class GoTListener(StreamListener): 
+
+    def on_data(self, data): 
+        try: 
+            with open('example_file.json', 'a') as f: 
+                f.write(data)
+                return True
+        except BaseException as e: 
+            print('Error on_data: {}'.format(e))
+
+    def on_error(self, status): 
+        print(status)
+        return True 
+
